dedd4c5278a9a9b28892ad11a54f5dd0d0c66e35
吴家勉 <wujiamian@ecqun.com>
2020-09-07 17:38:03 +0800

    添加gitignore跟bundle-analyzer

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,7 +1,11 @@
 const baseConfig = require('../config/webpack.config.base');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const { merge } = require('webpack-merge'); // 版本升级变成 {}
 const config  = merge(baseConfig, {
-    mode: 'production'
+    mode: 'production',
+    plugins: [
+        new BundleAnalyzerPlugin()
+    ]
 })
 
 module.exports =  config
--- view/App.tsx
+++ view/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const App = () => (
-    <div> 从零开始手写webpack搭建react项目 热更新</div>
+    <div> 从零开始手写webpack搭建react项目 热更新gitgnore</div>
 )
 
 export default App;
